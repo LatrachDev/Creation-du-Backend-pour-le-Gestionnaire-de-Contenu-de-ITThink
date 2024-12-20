219b887404238483b222ff53eb942f73d257f24d
LatrachDev <m.latrach.youcode@gmail.com>
2024-12-20 23:02:38 +0100

    edit dashboard

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -14,6 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
     <script src="Javascript/tailwind.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
         /* Hide sections by default */
         .dashboard-section {
